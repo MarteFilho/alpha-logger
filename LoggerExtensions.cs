6ecbe09136b9df9dbd9dc4141ea4b0854cedff7d
MarteFilho <marcosrogerionino1@hotmail.com>
2021-07-24 12:56:07 -0300

    First Commit

---
+++ LoggerExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Net.Http;
+
+namespace AlphaLogger.Common
+{
+    public static class LoggerExtensions
+    {
+        public static void LogHttpResponse(this ILogger logger, HttpResponseMessage response)
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                logger.LogDebug("Received a success response from {Url}", response.RequestMessage.RequestUri);
+            }
+            else
+            {
+                logger.LogWarning("Received a non-success status code {StatusCode} from {Url}",
+                    (int)response.StatusCode, response.RequestMessage.RequestUri);
+            }
+        }
+    }
+}
---
+++ Logging.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+using Serilog.Sinks.Elasticsearch;
+using System;
+
+namespace AlphaLogger.Common
+{
+    public static class Logging
+    {
+        public static Action<HostBuilderContext, LoggerConfiguration, IServiceProvider> ConfigureLogger =>
+           (hostingContext, loggerConfiguration, services) =>
+           {
+               var env = hostingContext.HostingEnvironment;
+
+               loggerConfiguration.MinimumLevel.Information()
+                   .Enrich.FromLogContext()
+                   .Enrich.WithProperty("ApplicationName", env.ApplicationName)
+                   .Enrich.WithProperty("EnvironmentName", env.EnvironmentName)
+                   .Enrich.WithExceptionDetails()
+                   .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                   .MinimumLevel.Override("System.Net.Http.HttpClient", LogEventLevel.Warning)
+                   .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+                   .WriteTo.Console();
+
+               if (hostingContext.HostingEnvironment.IsDevelopment())
+               {
+                   loggerConfiguration.MinimumLevel.Override(env.ApplicationName, LogEventLevel.Debug);
+               }
+
+               var elasticUrl = hostingContext.Configuration.GetValue<string>("Logging:ElasticUrl");
+
+               if (!string.IsNullOrEmpty(elasticUrl))
+               {
+                   loggerConfiguration.WriteTo.Elasticsearch(
+                       new ElasticsearchSinkOptions(new Uri(elasticUrl))
+                       {
+                           AutoRegisterTemplate = true,
+                           AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7,
+                           IndexFormat = $"{env.ApplicationName}-logs-{0:yyyy.MM.dd}",
+                           MinimumLogEventLevel = LogEventLevel.Debug
+                       });
+               }
+
+               var azureInstrumentationKey = hostingContext.Configuration.GetValue<string>("Logging:ApplicationInsights");
+
+               if (!string.IsNullOrEmpty(azureInstrumentationKey))
+               {
+                   loggerConfiguration.WriteTo.ApplicationInsights(services.GetRequiredService<TelemetryConfiguration>(),
+                    TelemetryConverter.Traces);
+               }
+           };
+    }
+}
